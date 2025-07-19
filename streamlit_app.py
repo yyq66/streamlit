783570cb83a894cae6cbca5fa8e223c2ea3fce73
yyq <2545468428@qq.com>
2025-07-19 23:40:12 +0800

    1

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 
-import sys
-import pysqlite3
-sys.modules['sqlite3'] = pysqlite3
+# import sys
+# import pysqlite3
+# sys.modules['sqlite3'] = pysqlite3
 
 import streamlit as st
 from zhipuai_embedding import ZhipuAIEmbeddings
